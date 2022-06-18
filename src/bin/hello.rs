a82de1ff346988e58cc44245568f8f6d63b34df5
Chevdor <chevdor@gmail.com>
2022-06-18 22:37:38 +0200

    Add temp and logger config

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -8,6 +8,7 @@ use nrf52840_hal::{
 	self as hal,
 	gpio::{p0::Parts as P0Parts, Level},
 	Timer,
+	Temp,
 }; // global logger + panicking-behavior + memory layout
 
 enum Color {
@@ -83,8 +84,11 @@ impl RgbLed {
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
+	defmt::info!("Starting...");
+
 	let board = hal::pac::Peripherals::take().unwrap();
 	let mut timer = Timer::new(board.TIMER0);
+	let mut temp = Temp::new(board.TEMP);
 
 	let p0_pins = P0Parts::new(board.P0);
 	let mut led_1 = p0_pins.p0_13.into_push_pull_output(Level::Low);
@@ -94,7 +98,7 @@ fn main() -> ! {
 		p0_pins.p0_28.into_push_pull_output(Level::High).into(),
 	);
 
-	let delay = 500u32;
+	let delay = 100u32;
 	loop {
 		led_1.set_high().unwrap();
 
@@ -119,13 +123,19 @@ fn main() -> ! {
 
 
 		timer.delay_ms(delay);
-		rgb.set_color(Color::White);
+		rgb.set_color(Color::Green);
 
 		timer.delay_ms(delay);
 		rgb.set_color(Color::Off);
 
 		led_1.set_low().unwrap();
 		timer.delay_ms(delay);
+
+		let temperature: f32 = temp.measure().to_num();
+		defmt::println!("{=f32} °C", temperature);
+		defmt::debug!("{=f32} °C", temperature);
+		defmt::info!("{=f32} °C", temperature);
+
 	}
 
 	// my_app::exit()
