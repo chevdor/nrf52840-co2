16c0d2757192f13b02a9b4bdf4abeda09e5fc1e0
Chevdor <chevdor@gmail.com>
2022-06-19 16:10:46 +0200

    refactor rgb led

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,86 +1,19 @@
 #![no_main]
 #![no_std]
+mod rgb_led;
 
 use embedded_hal::{blocking::delay::DelayMs, digital::v2::OutputPin};
 use hal::gpio::{Output, Pin, PushPull};
+
 use nrf52840_co2 as _;
 use nrf52840_hal::{
 	self as hal,
 	gpio::{p0::Parts as P0Parts, Level},
 	Timer,
 	Temp,
-}; // global logger + panicking-behavior + memory layout
+};
 
-enum Color {
-	Off,
-
-	Red,
-	Green,
-	Blue,
-
-	Yellow,
-	Cyan,
-	Magenta,
-
-	White,
-}
-
-struct RgbLed {
-	r: Pin<Output<PushPull>>,
-	g: Pin<Output<PushPull>>,
-	b: Pin<Output<PushPull>>,
-}
-
-impl RgbLed {
-	pub fn new(r: Pin<Output<PushPull>>, b: Pin<Output<PushPull>>, g: Pin<Output<PushPull>>) -> Self {
-		Self { r, g, b }
-	}
-
-	pub fn set_color(&mut self, color: Color) {
-		match color {
-			Color::Off => {
-				self.r.set_high().unwrap();
-				self.b.set_high().unwrap();
-				self.g.set_high().unwrap();
-			}
-			Color::Red => {
-				self.r.set_low().unwrap();
-				self.b.set_high().unwrap();
-				self.g.set_high().unwrap();
-			}
-			Color::Green => {
-				self.r.set_high().unwrap();
-				self.b.set_high().unwrap();
-				self.g.set_low().unwrap();
-			}
-			Color::Blue => {
-				self.r.set_high().unwrap();
-				self.b.set_low().unwrap();
-				self.g.set_high().unwrap();
-			}
-			Color::Magenta => {
-				self.r.set_low().unwrap();
-				self.b.set_low().unwrap();
-				self.g.set_high().unwrap();
-			}
-			Color::Yellow => {
-				self.r.set_low().unwrap();
-				self.b.set_high().unwrap();
-				self.g.set_low().unwrap();
-			}
-			Color::Cyan => {
-				self.r.set_high().unwrap();
-				self.b.set_low().unwrap();
-				self.g.set_low().unwrap();
-			}
-			Color::White => {
-				self.r.set_low().unwrap();
-				self.b.set_low().unwrap();
-				self.g.set_low().unwrap();
-			} // _ => {}
-		}
-	}
-}
+use crate::rgb_led::{RgbLed, Color}; // global logger + panicking-behavior + memory layout
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
