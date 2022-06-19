8a2ccf2391fcbbae53e2ea7a1ebb652da49cbb00
Chevdor <chevdor@gmail.com>
2022-06-19 19:03:32 +0200

    Add temp and temp unit switching

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -15,7 +15,7 @@ use nrf52840_hal::{
 use crate::{
 	button::Button,
 	rgb_led::{Color, RgbLed},
-	temperature::Temperature,
+	temperature::TemperatureUnit,
 }; // global logger + panicking-behavior + memory layout
 
 #[cortex_m_rt::entry]
@@ -40,43 +40,59 @@ fn main() -> ! {
 	);
 
 	let delay = 1000u32;
+	let mut current_unit = TemperatureUnit::Celsius;
+
 	loop {
 		led_1.set_high().unwrap();
 
 		if button_1.is_pressed() {
-			timer.delay_ms(delay / 10);
-			rgb.set_color(Color::Blue);
+			current_unit = match current_unit {
+				TemperatureUnit::Fahrenheit => TemperatureUnit::Kelvin,
+				TemperatureUnit::Kelvin => TemperatureUnit::Celsius,
+				TemperatureUnit::Celsius => TemperatureUnit::Fahrenheit,
+			};
+			// defmt::info!("Unit changed to {=?}", current_unit);
+			defmt::info!("Unit changed");
+		};
 
-			timer.delay_ms(delay / 10);
-			rgb.set_color(Color::White);
+		// if button_1.is_pressed() {
 
-			timer.delay_ms(delay / 10);
-			rgb.set_color(Color::Red);
+		timer.delay_ms(delay / 10);
+		rgb.set_color(Color::Blue);
 
-			timer.delay_ms(delay / 10);
-			rgb.set_color(Color::Yellow);
+		timer.delay_ms(delay / 10);
+		rgb.set_color(Color::White);
 
-			timer.delay_ms(delay / 10);
-			rgb.set_color(Color::Cyan);
+		timer.delay_ms(delay / 10);
+		rgb.set_color(Color::Red);
 
-			timer.delay_ms(delay / 10);
-			rgb.set_color(Color::Magenta);
+		timer.delay_ms(delay / 10);
+		rgb.set_color(Color::Yellow);
 
-			timer.delay_ms(delay / 10);
-			rgb.set_color(Color::Green);
+		timer.delay_ms(delay / 10);
+		rgb.set_color(Color::Cyan);
 
-			timer.delay_ms(delay / 10);
-			rgb.set_color(Color::Off);
-		} else {
-			timer.delay_ms(delay);
-		}
+		timer.delay_ms(delay / 10);
+		rgb.set_color(Color::Magenta);
+
+		timer.delay_ms(delay / 10);
+		rgb.set_color(Color::Green);
+
+		timer.delay_ms(delay / 10);
+		rgb.set_color(Color::Off);
+		// } else {
+		// 	timer.delay_ms(delay);
+		// }
 
 		led_1.set_low().unwrap();
 		timer.delay_ms(delay / 10);
 
 		let temperature: f32 = temp.measure().to_num();
-		defmt::info!("{=f32} °C", Temperature::new(temperature).to_celcius()); // we could have used `temperature` directly as well
-		defmt::info!("{=f32} °F", Temperature::new(temperature).to_fahrenheit());
-		defmt::info!("{=f32} °K", Temperature::new(temperature).to_kelvin());
+		match current_unit {
+			TemperatureUnit::Fahrenheit => defmt::info!("{=f32} °F", current_unit.convert_temperature(temperature)),
+			TemperatureUnit::Celsius => defmt::info!("{=f32} °C", current_unit.convert_temperature(temperature)),
+			TemperatureUnit::Kelvin => defmt::info!("{=f32} °K", current_unit.convert_temperature(temperature)),
+		};
 	}
 }
+                                                                                                        
--- src/bin/temperature.rs
+++ src/bin/temperature.rs
@@ -2,36 +2,67 @@
 // #![no_std]
 #![cfg_attr(not(test), no_std)]
 
+use defmt::{Format, Str};
 use nrf52840_co2 as _;
 use nrf52840_hal::{self as _};
 
-// pub enum TemperatureUnit {
-// 	Fahrenheit(f32),
-// 	Celsius(f32),
-// 	Kelvin(f32),
+pub enum TemperatureUnit {
+	Fahrenheit,
+	Celsius,
+	Kelvin,
+}
+
+// impl Format for TemperatureUnit {
+//     fn format(&self, fmt: defmt::Formatter) {\
+// 		let s = match self {
+//             TemperatureUnit::Fahrenheit => ("Fahrenheit"),
+//             TemperatureUnit::Celsius => "Celcius",
+//             TemperatureUnit::Kelvin => "Kelvin",
+//         };
+// 		defmt::export::istr(s.into());
+//         defmt::export::u8(s)
+// 	}
 // }
 
-pub struct Temperature {
-	celcius: f32, // Celcius
+// pub struct Temperature {
+// 	celcius: f32, // Celcius
+// }
+
+impl TemperatureUnit {
+    pub fn convert_temperature(&self, temperature: f32) -> f32 {
+        match self {
+            TemperatureUnit::Fahrenheit => {
+                temperature * 1.8 + 32f32
+            },
+
+            TemperatureUnit::Kelvin => {
+                temperature + 273.15f32
+            },
+
+            TemperatureUnit::Celsius => {
+                temperature
+            },
+        }
+    }
 }
 
-impl Temperature {
-	pub fn new(celcius: f32) -> Self {
-		Self { celcius }
-	}
+// impl Temperature {
+// 	pub fn new(celcius: f32) -> Self {
+// 		Self { celcius }
+// 	}
 
-	pub fn to_fahrenheit(&self) -> f32 {
-		self.celcius * 1.8 + 32f32
-	}
+// 	pub fn to_fahrenheit(&self) -> f32 {
+// 		self.celcius * 1.8 + 32f32
+// 	}
 
-	pub fn to_celcius(&self) -> f32 {
-		self.celcius
-	}
+// 	pub fn to_celcius(&self) -> f32 {
+// 		self.celcius
+// 	}
 
-	pub fn to_kelvin(&self) -> f32 {
-		self.celcius + 273.15f32
-	}
-}
+// 	pub fn to_kelvin(&self) -> f32 {
+// 		self.celcius + 273.15f32
+// 	}
+// }
 
 // #[cfg(test)]
 // mod test_super {
