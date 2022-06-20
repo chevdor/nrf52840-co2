592c5320e51f212df0cdeaf579c11386358c725f
Chevdor <chevdor@gmail.com>
2022-06-20 22:54:18 +0200

    Reintroduce the blinky with RTIC and specify probe

--- justfile
+++ justfile
@@ -1,4 +1,3 @@
-
 # List available commands
 _default:
   just --choose --chooser "fzf +s -x --tac --cycle"
@@ -9,3 +8,8 @@ setup:
 	cargo install cargo-generate
 	cargo install flip-link
 
+show_probes:
+	cargo flash --list-probes
+
+run_rtic:
+	cargo run --bin rtic -- --probe $VID:$PID:$SERIAL
--- src/bin/rtic.rs
+++ src/bin/rtic.rs
@@ -12,6 +12,9 @@ mod app {
 		hal::{
 			gpio::{Input, Level, Output, Pin, PullUp, PushPull},
 			gpiote::*,
+			// PPI allows telling the microcontroller how to handle the perfipherals, without interefering with the cpu
+			// https://infocenter.nordicsemi.com/index.jsp?topic=%2Fcom.nordic.infocenter.nrf52832.ps.v1.1%2Fppi.html
+			ppi::{self, ConfigurablePpi, Ppi},
 		},
 		nrf52840_hal as hal,
 	};
@@ -25,8 +28,9 @@ mod app {
 	struct Local {
 		btn_1: Pin<Input<PullUp>>,
 		btn_2: Pin<Input<PullUp>>,
-		led1: Pin<Output<PushPull>>,
-		led1_state: bool,
+		btn_3: Pin<Input<PullUp>>,
+		btn_4: Pin<Input<PullUp>>,
+		// led1 is managed via ppi
 		led2: Pin<Output<PushPull>>,
 		led2_state: bool,
 	}
@@ -35,7 +39,7 @@ mod app {
 	type MonoTimer = Systick<1000>;
 
 	#[init]
-	fn init(mut ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+	fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
 		let port_0 = hal::gpio::p0::Parts::new(ctx.device.P0);
 
 		let led1 = port_0.p0_13.into_push_pull_output(Level::High).degrade();
@@ -44,40 +48,51 @@ mod app {
 
 		let btn_1 = port_0.p0_11.into_pullup_input().degrade();
 		let btn_2 = port_0.p0_12.into_pullup_input().degrade();
+		let btn_3 = port_0.p0_24.into_pullup_input().degrade();
+		let btn_4 = port_0.p0_25.into_pullup_input().degrade();
 
 		// see https://github.com/nrf-rs/nrf-hal/blob/663008c033ad67263e4ac0c561d5d1fac28d7170/examples/gpiote-demo/src/main.rs
 		let gpiote = Gpiote::new(ctx.device.GPIOTE);
 
 		// Set btn1 to generate event on channel 0 and enable interrupt
 		gpiote.channel0().input_pin(&btn_1).hi_to_lo().enable_interrupt();
-		gpiote.port().input_pin(&btn_2).low();
+		gpiote.port().input_pin(&btn_3).low();
+		gpiote.port().input_pin(&btn_4).low();
 
 		// Enable interrupt for port event
 		gpiote.port().enable_interrupt();
 
+		// PPI usage, channel 2 event triggers "task out" operation (toggle) on channel 1 (toggles led1)
+		gpiote.channel1().output_pin(led1).task_out_polarity(TaskOutPolarity::Toggle).init_high();
+		gpiote.channel2().input_pin(&btn_2).hi_to_lo();
+		let ppi_channels = ppi::Parts::new(ctx.device.PPI);
+		let mut ppi0 = ppi_channels.ppi0;
+		ppi0.set_task_endpoint(gpiote.channel1().task_out());
+		ppi0.set_event_endpoint(gpiote.channel2().event());
+		ppi0.enable();
+
 		#[cfg(feature = "semihosting")]
 		hprintln!("starting").unwrap();
 
-		// blink1::spawn_after(Duration::<u64, 1, 1000>::from_ticks(100)).unwrap();
-		(
-			Shared { gpiote },
-			Local { btn_1, btn_2, led1, led1_state: false, led2, led2_state: false },
-			init::Monotonics(mono),
-		)
+		defmt::println!("Now press the buttons 1,2,3 and 4. Observe how the output and how pressing buttons 1 and 2 toggle the LED where pressing 3 and 4 only turn it on");
+		blink1::spawn_after(Duration::<u64, 1, 1000>::from_ticks(100)).unwrap();
+		(Shared { gpiote }, Local { btn_1, btn_2, btn_3, btn_4, led2, led2_state: false }, init::Monotonics(mono))
 	}
 
-	// #[task(local = [led1, led1_state])]
-	// fn blink1(cx: blink1::Context) {
-	// 	if *cx.local.led1_state {
-	// 		let _ = cx.local.led1.set_low();
-	// 		*cx.local.led1_state = false;
-	// 	} else {
-	// 		let _ = cx.local.led1.set_high();
-	// 		*cx.local.led1_state = true;
-	// 	}
+	#[task(local = [led2, led2_state])]
+	fn blink1(cx: blink1::Context) {
+		if *cx.local.led2_state {
+			// let mut led2 = cx.local.led2.into_push_pull_output(Level::High);
+			let _ = hal::prelude::OutputPin::set_high(cx.local.led2);
+			*cx.local.led2_state = false;
+		} else {
+			// let mut led2 = cx.local.led2.into_push_pull_output(Level::High);
+			let _ = hal::prelude::OutputPin::set_low(cx.local.led2);
+			*cx.local.led2_state = true;
+		}
 
-	// 	blink1::spawn_after(Duration::<u64, 1, 1000>::from_ticks(250)).unwrap();
-	// }
+		blink1::spawn_after(Duration::<u64, 1, 1000>::from_ticks(250)).unwrap();
+	}
 
 	#[task(binds = GPIOTE, shared = [gpiote])]
 	fn on_gpiote(mut ctx: on_gpiote::Context) {
@@ -95,10 +110,12 @@ mod app {
 		});
 	}
 
-	#[task(shared = [gpiote], local = [btn_1, btn_2])]
+	#[task(shared = [gpiote], local = [btn_1, btn_2, btn_3, btn_4])]
 	fn debounce(mut ctx: debounce::Context) {
 		let btn1_pressed = ctx.local.btn_1.is_low().unwrap();
 		let btn2_pressed = ctx.local.btn_2.is_low().unwrap();
+		let btn3_pressed = ctx.local.btn_3.is_low().unwrap();
+		let btn4_pressed = ctx.local.btn_4.is_low().unwrap();
 
 		ctx.shared.gpiote.lock(|gpiote| {
 			if btn1_pressed {
@@ -108,6 +125,16 @@ mod app {
 			}
 			if btn2_pressed {
 				defmt::println!("Button 2 was pressed!");
+				// Manually run "task clear" on channel 1 (led1 on)
+				gpiote.channel1().clear();
+			}
+			if btn3_pressed {
+				defmt::println!("Button 3 was pressed!");
+				// Manually run "task clear" on channel 1 (led1 on)
+				gpiote.channel1().clear();
+			}
+			if btn4_pressed {
+				defmt::println!("Button 4 was pressed!");
 				// Manually run "task clear" on channel 1 (led1 on)
 				gpiote.channel1().clear();
 			}
